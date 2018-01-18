f15a85328114aa18d56f56a1366fd899b3a067c1
Jeremy Zhou <zhou.jeremy.2001@gmail.com>
2018-01-18 12:52:47 -0500

    Update Final.java

--- Final.java
+++ Final.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class Final {
 	public static void main(String[] args) throws IOException{
-		
+
 		//Input
 		Scanner reader = new Scanner(new File("final.in"));
 		int n = reader.nextInt();
@@ -19,37 +19,50 @@ public class Final {
 			}
 		}
 		reader.close();
-		
+
 		//Normal Matrix Multiplication
-		long time = System.currentTimeMillis();
+		long time = System.nanoTime();
 		double[][] result1 = mult(test1, test2);
-		long time1 = System.currentTimeMillis() - time;
+		long time1 = System.nanoTime() - time;
 		printMatrix(result1);
-		
-		
+
 		//Matrix multiplication with diagonalization
-		time = System.currentTimeMillis();
+		time = System.nanoTime();
 		double[][] result2 = diagMult(test1, test2);
-		long time2 = System.currentTimeMillis() - time;
+		long time2 = System.nanoTime() - time;
 		printMatrix(result2);
-		
+
 		//Output
 		System.out.println("Normal Multiplication Time: " + time1);
 		System.out.println("Diagonilization Multiplication Time:" + time2);
 	}
-	
+
 	//Normal Matrix Multiplication
 	public static double[][] mult(double[][] mat1, double[][] mat2) {
-		//TODO
-		return null;
+		int n = mat1.length;
+		double value = 0;
+		double[] a, double[] b;
+		double[][] result = new double[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				value = 0;
+				a = mat1[i];
+				for (int k = 0; k < n; k++)	b[k] = mat2[k][j];
+				for (int k = 0; i < n; k++)	value += a[k]*b[k];
+				result[i][j] = value;
+			}
+		}
+		return result;
 	}
-	
+
 	//Matrix multiplication with diagonalization
 	public static double[][] diagMult(double[][] mat1, double[][] mat2) {
-		//TODO
-		return null;
+		int n = mat1.length;
+		double[][] result = new double[n][n];
+		\\TODO
+		return result;
 	}
-	
+
 	//Prints a 2d matrix
 	public static void printMatrix(double[][] mat) {
 		for (int i = 0; i < mat.length; i++) {
